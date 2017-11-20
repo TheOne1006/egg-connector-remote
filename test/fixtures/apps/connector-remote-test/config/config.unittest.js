dc6c83b691042cb95b5fbd3c6512a4667d206c88
theone <297190869@qq.com>
2017-11-20 18:10:56 +0800

    test: travis ci ,start mysql server

--- test/fixtures/apps/connector-remote-test/config/config.unittest.js
+++ test/fixtures/apps/connector-remote-test/config/config.unittest.js
@@ -6,7 +6,7 @@ module.exports = () => {
       port: '3306',
       host: '127.0.0.1',
       username: 'root',
-      password: 'password',
+      password: '',
       database: 'test',
       dialect: 'mysql',
       pool: {
