46a191ed2b954012e616562db0f98695e7efb312
theone <297190869@qq.com>
2017-12-29 18:24:57 +0800

    feat: xxxRemote 移到 nextFeature

---
+++ test/lib/registerRemote/helper.test.js
@@ -0,0 +1,6 @@
+// 'use strict';
+// const assert = require('assert');
+// const createSwagger = require('../../../lib/registerRemote/helper');
+//
+// describe('test/lib/registerRemote/helper.js', () => {
+// });
